ba296810efd46c9e6b3a85ce7903394e46a03bad
pjsnps <pjalajas@synopsys.com>
2020-08-12 12:30:30 -0400

    CHANGELOG:  2008121627Z pj Add sleep command line positional parameter .  Add advanced usage, with new version of SnpsDbStatDelta_202008.bash which accepts input via pipe from output of this script. Change shebang to /usr/bin/bash. Fix typo. Remove customer name.

--- SynopsysMonitorDbActivity_202007.bash
+++ SynopsysMonitorDbActivity_202007.bash
@@ -4,15 +4,16 @@
 #AUTHOR:     pjalajas@synopsys.com
 #LICENSE:    SPDX Apache-2.0
 #CREATED:    2020-07-24          # pj move from command line dev to script
-#VERSION:    ____         # :! date -u +\%y\%m\%d\%H\%MZ
-#GREPVCKSUM: ____ under dev# :! grep -v grepvcksum <script> | cksum
-#CHANGELOG:  ___ pj Add sleep command line positional parameter $1.  Add advanced usage, with new version of SnpsDbStatDelta_202008.bash which accepts input via pipe from output of this script. Change shebang to /usr/bin/bash.
+#VERSION:    2008121627Z         # :! date -u +\%y\%m\%d\%H\%MZ
+#GREPVCKSUM: 2841540408 12312    # :! grep -v grepvcksum <script> | cksum
+#CHANGELOG:  2008121627Z pj Add sleep command line positional parameter $1.  Add advanced usage, with new version of SnpsDbStatDelta_202008.bash which accepts input via pipe from output of this script. Change shebang to /usr/bin/bash. Fix typo. Remove customer name. 
  
 #PURPOSE:    To try to detect database activity during long silent software upgrades, database migrations, etc.  
-#PURPOSE:    Meant to only be used maybe every hour or two during an event, just to make sure the database is still processing data silently. 
+#PURPOSE:    Meant to only be used maybe every hour or two during an upgrade or similar Black Duck event, just to make sure the database is still processing data silently. 
+#PURPOSE:    Suggestions welcome. 
 
-#REQUIRES:  psql, Black Duck server.
-#RECOMMENDED:  ./SnpsDbStatDelta_202008.bash to calculate Deltas between two runs of stats commands. 
+#REQUIRES:  psql, Black Duck (FKA Hub) server.
+#RECOMMENDED:  SnpsDbStatDelta_202008.bash to calculate deltas between two runs of these db stats commands. 
 
 #USAGE: Edit CONFIGs, then:
 #USAGE: SynopsysMonitorDbActivity_202007.bash 15s
@@ -73,6 +74,7 @@ do
   echo
   echo pg_stat_user_indexes... 
   echo "SELECT now(), inet_server_addr(), 'pg_stat_user_indexes' AS cmd, $loop AS loop, * FROM pg_stat_user_indexes \
+      --take out WHERE clause to see everything, for the moment
       --WHERE relname IN ('v_hub','v_bootstrap','scan_composite_leaf','notification','audit_event','scan_scan','scan_file') \
       ; " | \
     $PSQL $DBCONN ; 
@@ -80,6 +82,7 @@ do
   echo
   echo pg_statio_user_sequences... 
   echo "SELECT now(), inet_server_addr(), 'pg_statio_user_sequences' AS cmd, $loop AS loop, * FROM pg_statio_user_sequences \
+      --take out WHERE clause to see everything, for the moment
       --WHERE relname IN ('v_hub','v_bootstrap','scan_composite_leaf','notification','audit_event','scan_scan','scan_file') \
       ; " | \
     $PSQL $DBCONN ; 
@@ -130,7 +133,7 @@ exit
 #REFERENCE
 put notes here
 
-Can use any one (only one for now) of these "cmd" in grep -zPo filter:
+Can use any one (only one for now) of these "cmd" in grep -zPo filter, though some may have no data of which to take deltas:
 [pjalajas@sup-pjalajas-hub SynopsysScripts]$ grep -Po "\'.*\' AS cmd" ./SynopsysMonitorDbActivity_202007.bash | tr -d \' | sed -re 's/ AS cmd//g'                             
 v_hub_script_status
 v_bootstrap_script_status
@@ -179,7 +182,7 @@ now                            |  inet_server_addr  |  cmd                    | 
 
 
 
-Just show the deltas for bds_hub db:
+Just show the deltas for pg_stat_database for bds_hub db:
 [pjalajas@sup-pjalajas-hub 00811525_Install2020.6.0]$ ./SynopsysMonitorDbActivity_202007.bash |& grep -zPo '(?s)^pg_stat_database\.\.\..*?rows\)' |& ./SnpsDbStatDelta_202008.bash | cat -A | grep -e "^now" -e bds_hub\ 
 now         |  inet_server_addr  |  cmd               |  loop  |  datid  |  datname         |  numbackends  |  xact_commit  |  xact_rollback  |  blks_read  |  blks_hit  |  tup_returned  |  tup_fetched  |  tup_inserted  |  tup_updated  |  tup_deleted  |  conflicts  |  temp_files  |  temp_bytes  |  deadlocks  |  blk_read_time  |  blk_write_time  |  stats_reset$
 18_seconds  |  10.0.0.66         |  pg_stat_database  |  1     |  .      |  bds_hub         |  .            |  538          |  .              |  .          |  10093     |  127820        |  3079         |  .             |  9            |  .            |  .          |  .           |  .           |  .          |  .              |  .               |  0_seconds$
