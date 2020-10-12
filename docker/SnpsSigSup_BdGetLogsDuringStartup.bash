0ec9b4d6fe15bfb8a785595b1da5d9e9b91d66de
pjsnps <pjalajas@synopsys.com>
2020-10-12 16:57:33 -0400

    mod SnpsSigSup_BdGetLogsDuringStartup.bash

--- docker/SnpsSigSup_BdGetLogsDuringStartup.bash
+++ docker/SnpsSigSup_BdGetLogsDuringStartup.bash
@@ -1,6 +1,18 @@
 #!/usr/bin/bash
+#SCRIPT: SnpsSigSup_BdGetLogsDuringStartup.bash 
+#DATE: Mon Oct 12 20:53:19 UTC 2020
+#AUTHOR: pjalajas@synopsys.com
+#SUPPORT: https://community.synopsys.com/s/ Software-integrity-support@synopsys.com
+#LICENSE: SPDX Apache-2.0 https://spdx.org/licenses/Apache-2.0.html
+#VERSION: 2010122053Z
+#GREPVCKSUM: TODO 
 
+#PURPOSE: Provides different ways to view container logs 
 
+#SnpsSigSup_BdGetLogsDuringStartup.bash 2 | less -inRF
+
+#USAGE: #1. Quick tail -n 1 of each active container.       bash SnpsSigSup_BdGetLogsDuringStartup.bash 1 | less -inRF
+#USAGE: #2. Full logs of most recent 10 Exited containers.  bash SnpsSigSup_BdGetLogsDuringStartup.bash 2 | less -inRF
 
 #TODO protect numbers 
 #TODO: put container name in output line instead of containerid.  
