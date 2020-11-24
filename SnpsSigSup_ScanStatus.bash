07fab5d8a44d89f6fd42726a38c048ab5cbfb338
pjsnps <pjalajas@synopsys.com>
2020-11-23 21:33:15 -0500

    SnpsSigSup_ScanStatus.bash mod

--- SnpsSigSup_ScanStatus.bash
+++ SnpsSigSup_ScanStatus.bash
@@ -1,21 +1,89 @@
 #!/bin/bash
 #SCRIPT:  SnpsSigSup_ScanStatus.bash
+#AUTHOR:  M. Zaid Alam, pjalajas@synopsys.com
+#LICENSE: SPDX Apache-2.0
+#SUPPORT: https://www.synopsys.com/software-integrity/support.html, https://community.synopsys.com/s/, Software-integrity-support@synopsys.com
+#DATE:   2020-11-23
+#VERSION: 2011240152Z
 
-#USAGE: #[pjalajas@sup-pjalajas-hub SynopsysScripts]$ bash ./SnpsSigSup_ScanStatus.bash -u https://sup-pjalajas-hub.dc1.lan -a 'Nzg1ZjEyZjctM2RjZC00Y2Y3LTkxNWItZTgyMjJiOGJmYTVhOjYzYmRlODNjLTRmNTEtNDA0OS1iYmY3LTc3ZDVjOTkxYTU2Ng==' -p PN-pj-test-cloned -v PVN_1109212443Z --consider-snippets false
+: '
+
+
+[pjalajas@sup-pjalajas-hub git]$ docker container exec -upostgres adb543463eba psql -x -d bds_hub -c "SELECT * FROM st.scan_scan WHERE code_location_id = '831415ed-15a6-4a72-b887-00af34c274c8' LIMIT 5 ; "
+-[ RECORD 1 ]------+--------------------------------------------
+basedir            | /
+host_name          | sup-pjalajas-hub.dc1.lan
+scantime           | 1606161907213
+timelastmodified   | 1606161907594
+timetopersistms    | 0
+timetoscanms       | 0
+scan_source_type   | SN
+scan_source_id     | 89e26127-d735-4203-a30b-d53073d2268d
+id                 | 89e26127-d735-4203-a30b-d53073d2268d
+scanner_version    | 2020.10.0
+created_by_user_id | 00000000-0000-0000-0001-000000000001
+signature_version  | 7.0.0
+status             | SCANNING
+match_count        | 0
+num_non_dir_files  | 0
+num_dirs           | 0
+file_system_size   | 
+status_message     | 
+name               | vmdk/PN-pj-test-cloned/PVN_1109212443Z scan
+type               | FS
+code_location_id   | 831415ed-15a6-4a72-b887-00af34c274c8
+server_version     | 2020.10.0
+
+BUT, Scan not still running:
+
+[pjalajas@sup-pjalajas-hub git]$ docker container exec -upostgres adb543463eba psql -x -d bds_hub -c "SELECT * FROM pg_stat_activity WHERE query LIKE '%scan_%' AND application_name <> 'psql'  ; " | less -inRF
+(0 rows)
+__TODO but how to make sure some other scan is not hitting scan_ tables...
+
+
+
+
+[pjalajas@sup-pjalajas-hub hub-rest-api-python]$ docker container exec -upostgres adb543463eba psql -x -d bds_hub -c "SELECT * FROM st.scan_scan WHERE code_location_id = 'c559fcf3-f424-4629-aa63-cd8125850333' LIMIT 5 ; "                                                                                                                                                                                                                  
+-[ RECORD 1 ]------+---------------------------------------------------------------------------------------
+basedir            | /
+host_name          | sup-pjalajas-hub.dc1.lan
+scantime           | 1605651979457
+timelastmodified   | 1605651980292
+timetopersistms    | 0
+timetoscanms       | 0
+scan_source_type   | SN
+scan_source_id     | e4d286ad-94d6-4d53-9de9-4ff9ac7e3991
+id                 | e4d286ad-94d6-4d53-9de9-4ff9ac7e3991
+scanner_version    | 2020.10.0
+created_by_user_id | 00000000-0000-0000-0001-000000000001
+signature_version  | 7.0.0
+status             | ERROR
+match_count        | 0
+num_non_dir_files  | 0
+num_dirs           | 0
+file_system_size   | 
+status_message     | /home/pjalajas/Documents/dev/hub/test/PN_pj_project_name_comman
+name               | PN_pj_project_name_command_line_input5/PN_pj_project_name_command_line_input4/1.0 scan
+type               | FS
+code_location_id   | c559fcf3-f424-4629-aa63-cd8125850333
+server_version     | 2020.10.0
+'
 
 # Copyright (c) 2020 Synopsys, Inc. All rights reserved worldwide. The information
 # contained in this file is the proprietary and confidential information of
 # Synopsys, Inc. and its licensors, and is supplied subject to, and may be used
 # only by Synopsys customers in accordance with the terms and conditions of a
 # previously executed license agreement between Synopsys and that customer.
-#
 # Author: M. Zaid Alam
-#
 # Version: 2020.06.19
-#
+
+
+
+#USAGE: #[pjalajas@sup-pjalajas-hub SynopsysScripts]$ bash ./SnpsSigSup_ScanStatus.bash -u https://sup-pjalajas-hub.dc1.lan -a 'Nzg1ZjEyZjctM2RjZC00Y2Y3LTkxNWItZTgyMjJiOGJmYTVhOjYzYmRlODNjLTRmNTEtNDA0OS1iYmY3LTc3ZDVjOTkxYTU2Ng==' -p PN-pj-test-cloned -v PVN_1109212443Z --consider-snippets false
+
+
 # FUNCTION:
 # The script is intended to export a bom along with any changes made to it
-#
 # ALGORITIHM:
 # - Authenticate
 # - Find the given project by name
@@ -23,13 +91,12 @@
 # - Get the signature scans attached to a project version
 # - Process match counts
 # - if snippet is enabled also count unique files confirmed by snippets
-#
 # Usage:
 # ./get_match_coverage.sh -u <blackduck url> -a <api token> -p <project name> -v <version> --consider-snippets <true|false>
-#
-#
 # Example:
 # ./get_match_coverage.sh -u https://myblackduck -a top-secret-api-token -p example-project -v 1 --consider-snippets true
+
+
 
 
 # Globals for input
