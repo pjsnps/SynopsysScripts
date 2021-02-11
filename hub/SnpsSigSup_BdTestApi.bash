7fcf7750a003412f917c6ccb3441230ef3877f56
pjsnps <pjalajas@synopsys.com>
2021-02-11 09:33:26 -0500

    daily

--- hub/SnpsSigSup_BdTestApi.bash
+++ hub/SnpsSigSup_BdTestApi.bash
@@ -11,17 +11,25 @@
 
 
 #CONFIG: #################
+#BD_API_METHOD="GET" ; BD_API_REQUEST="/api/projects/7d104f09-3273-448c-81dc-e33a7674da8f/versions/a145ed5a-a241-47a2-8ff7-2aa1a8fecb7b/components/7a54f575-7f1a-4414-9d4d-6bc0e7a9224d/versions/a69b12a0-c9b9-4f74-ad63-de8bac977b32/policy-status"
+#Request URL: https://sup-pjalajas-2.dc1.lan/api/projects/55d6d2b4-2c28-485d-ab76-8bb6bab9e819/versions/c3780340-f560-455c-a150-c20431a6b728/components/907300e1-d66e-45ef-b77d-a790ed30b4e1/versions/a7d7dfd8-ef3c-4a2b-a04d-5b432eaccb7d/policy-status?limit=100
+
 projectId="7d104f09-3273-448c-81dc-e33a7674da8f"
+projectId="55d6d2b4-2c28-485d-ab76-8bb6bab9e819"
 projectVersionId="a145ed5a-a241-47a2-8ff7-2aa1a8fecb7b"
+projectVersionId="c3780340-f560-455c-a150-c20431a6b728"
 componentId="7a54f575-7f1a-4414-9d4d-6bc0e7a9224d"
+componentId="907300e1-d66e-45ef-b77d-a790ed30b4e1"
 componentVersionId="a69b12a0-c9b9-4f74-ad63-de8bac977b32"
+componentVersionId="a7d7dfd8-ef3c-4a2b-a04d-5b432eaccb7d"
 scan="598cecc4-0a7e-46e9-96ba-5a63be41ccf1"  # application/vnd.blackducksoftware.scan-4+json
 scanId=${scan}
 codeLocationId="15d3370b-3b88-4dbc-acdd-e7f86cca8ab5"  # application/vnd.blackducksoftware.scan-4+json
 
 #BD_TOKEN="ODcxYzA0OTAtNmQ4NC00NzhjLTk0MzctYzUwODJlNGMwOGY3OjMyYzk2ZWQ4LTIzZDYtNGVjNi1hZmIxLTNmYWQyZWUzYWNiOA=="
-BD_TOKEN="MTM3NWQ3Y2MtYTgyNS00ODBjLTkyNGItMzMzNjU0MmFmYzIwOjc4YzM2ZmMzLWYxZDktNDRlYy04ZGFhLTE1OTYwMDYxMjQ1Nw=="
-BD_TOKEN="ZWIyM2YzYjctYTlkOS00NjY3LThkZDktM2QxM2NhMTJhZjA0OjVkNThmMmQ3LTE0MmQtNDA0Zi1iM2YwLTNkNWRjYTM3MmM5YQ=="
+#BD_TOKEN="MTM3NWQ3Y2MtYTgyNS00ODBjLTkyNGItMzMzNjU0MmFmYzIwOjc4YzM2ZmMzLWYxZDktNDRlYy04ZGFhLTE1OTYwMDYxMjQ1Nw=="
+#BD_TOKEN="ZWIyM2YzYjctYTlkOS00NjY3LThkZDktM2QxM2NhMTJhZjA0OjVkNThmMmQ3LTE0MmQtNDA0Zi1iM2YwLTNkNWRjYTM3MmM5YQ=="
+BD_TOKEN="NjVhYjVlZDgtNjc4OC00NzY0LTg4NWEtNjk4Y2VkMzgyOWY0OjY5ZmQwMmFhLWVjZTUtNDA1ZS1hYWM2LTYyZmVkM2I0ZGVlZA=="
 #BD_URL="https://sup-pjalajas-hub.dc1.lan"  # with httpx, no trailing slash
 BD_URL="https://sup-pjalajas-2.dc1.lan"  # with httpx, no trailing slash
 BD_API_TOKENS_AUTH="/api/tokens/authenticate"  # no trailing slash
@@ -88,6 +96,10 @@ BD_API_METHOD="GET" ; BD_API_REQUEST="/api/notifications" ; BD_API_REQUEST_CONTE
 BD_API_METHOD="GET" ; BD_API_REQUEST="/api/notifications" ; BD_API_REQUEST_CONTENT_TYPE="application/vnd.blackducksoftware.notification-4+json"
 BD_API_METHOD="GET" ; BD_API_REQUEST="/api/notifications?filter=notificationType:policy_override" ; BD_API_REQUEST_CONTENT_TYPE="application/vnd.blackducksoftware.notification-4+json"
 BD_API_METHOD="GET" ; BD_API_REQUEST="/api/notifications-filters?filterKey=notificationType" ; BD_API_REQUEST_CONTENT_TYPE="application/vnd.blackducksoftware.notification-4+json"  # Lists notificationType's
+#GET /api/projects/{projectId}/versions/{projectVersionId}/components/{componentId}/versions/{componentVersionId}/policy-status Accept: application/vnd.blackducksoftware.bill-of-materials-6+json
+#BD_API_METHOD="GET" ; BD_API_REQUEST="/api/projects/7d104f09-3273-448c-81dc-e33a7674da8f/versions/a145ed5a-a241-47a2-8ff7-2aa1a8fecb7b/components/7a54f575-7f1a-4414-9d4d-6bc0e7a9224d/versions/a69b12a0-c9b9-4f74-ad63-de8bac977b32/policy-status"
+BD_API_METHOD="GET" ; BD_API_REQUEST="/api/projects/${projectId}/versions/${projectVersionId}/components/${componentId}/versions/${componentVersionId}/policy-status" ; BD_API_REQUEST_CONTENT_TYPE="content-type: application/vnd.blackducksoftware.internal-1+json"
+BD_API_METHOD="GET" ; BD_API_REQUEST="/api/projects/${projectId}/versions/${projectVersionId}/components/${componentId}/versions/${componentVersionId}/policy-status" ; BD_API_REQUEST_CONTENT_TYPE="application/vnd.blackducksoftware.bill-of-materials-6+json"
 
 # INIT
 
@@ -124,6 +136,9 @@ exit
 
 
 #REFERENCE ###############
+: '
+{"approvalStatus":"IN_VIOLATION_OVERRIDDEN","policyRuleViolationViews":[{"policyRuleView":{"name":"pj policy","enabled":true,"overridable":true,"severity":"UNSPECIFIED","expression":{"operator":"AND","expressions":[{"name":"SINGLE_VERSION","operation":"EQ","parameters":{"values":["https://sup-pjalajas-2.dc1.lan/api/components/907300e1-d66e-45ef-b77d-a790ed30b4e1"],"data":[{"componentName":"Bouncy Castle","componentUrl":"https://sup-pjalajas-2.dc1.lan/api/components/907300e1-d66e-45ef-b77d-a790ed30b4e1"}]},"displayName":"Component","developerScanExpression":true}]},"category":"UNCATEGORIZED","createdAt":"2021-02-10T20:58:37.003Z","createdBy":"sysadmin","createdByUser":"https://sup-pjalajas-2.dc1.lan/api/users/00000000-0000-0000-0001-000000000001","updatedAt":"2021-02-10T20:58:37.003Z","updatedBy":"sysadmin","updatedByUser":"https://sup-pjalajas-2.dc1.lan/api/users/00000000-0000-0000-0001-000000000001","_meta":{"allow":["DELETE","GET","PUT"],"href":"https://sup-pjalajas-2.dc1.lan/api/policy-rules/9e9a990c-bcab-4636-95be-53bf582a9cdf","links":[]}},"approvalStatus":"IN_VIOLATION_OVERRIDDEN","approvalStatusUrl":"https://sup-pjalajas-2.dc1.lan/api/projects/55d6d2b4-2c28-485d-ab76-8bb6bab9e819/versions/c3780340-f560-455c-a150-c20431a6b728/components/907300e1-d66e-45ef-b77d-a790ed30b4e1/versions/a7d7dfd8-ef3c-4a2b-a04d-5b432eaccb7d/policy-rules/9e9a990c-bcab-4636-95be-53bf582a9cdf/policy-status","overriddenBy":[{"user":"https://sup-pjalajas-2.dc1.lan/api/users/00000000-0000-0000-0001-000000000001","name":"sysadmin"}],"updatedBy":[{"user":"https://sup-pjalajas-2.dc1.lan/api/users/00000000-0000-0000-0001-000000000001","name":"sysadmin","comment":"pjalajaspolicyoverridecomment","updatedAt":"2021-02-10T20:59:39.376Z"}]}],"_meta":{"allow":["GET","PUT"],"href":"https://sup-pjalajas-2.dc1.lan/api/projects/55d6d2b4-2c28-485d-ab76-8bb6bab9e819/versions/c3780340-f560-455c-a150-c20431a6b728/components/907300e1-d66e-45ef-b77d-a790ed30b4e1/versions/a7d7dfd8-ef3c-4a2b-a04d-5b432eaccb7d/policy-status"}}
+'
 
 : '
   #-u "sysadmin:blackduck" \
